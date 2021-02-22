8160c219acb313b09404f74166afd275905b6131
Nathaniel_Kerr <NathanielKerr10@gmail.com>
2021-02-22 15:38:14 -0800

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,11 +4,11 @@ require 'active_support/inflector'
 class InteractiveRecord
 
     def self.table_name
-        "#{self.to_s.downcase}s"
+        "#{self.to_s.downcase.pluralize}"
     end
 
     def self.column_names
-        # DB[:conn].results_as_hash = true
+        DB[:conn].results_as_hash = true
 
         sql = "pragma table_info('#{table_name}')"
 
@@ -18,7 +18,7 @@ class InteractiveRecord
         column_names.compact
     end
     
-    def initialize(options = {})
+    def initialize(options={})
     options.each do |key, value|
         self.send("#{key}=", value)
         end
@@ -41,7 +41,6 @@ class InteractiveRecord
     end
 
     def save
-        
         sql = <<-SQL
         INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
         VALUES (#{values_for_insert})
@@ -49,10 +48,12 @@ class InteractiveRecord
 
         DB[:conn].execute(sql)
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0] 
+        
     end
 
     def self.find_by_name(name)
-        DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", name)
+        sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+        DB[:conn].execute(sql, name)
     end
 
     def self.find_by(name)
